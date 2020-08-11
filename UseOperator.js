534776f00dbe7941317cc2c751a1775758948b1f
MTLucifer <nguyenthanhtien.vnuf@gmail.com>
2020-08-11 09:03:53 +0700

    new

---
+++ UseOperator.js
@@ -0,0 +1,8 @@
+let inputWidht;
+let inputHeight;
+inputWidht = prompt ("Enter the width");
+inputHeight = prompt ("Enter the height");
+let width = parseInt(inputWidht);
+let height = parseInt(inputHeight);
+let area = width * height
+document.write("The area is: " + area)
